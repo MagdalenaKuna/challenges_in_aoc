4f3f34a49634af0fc2a4d58013b647d1f090bc86
Magdalena Kuna <madziakunia@gmail.com>
2024-12-02 16:07:03 +0100

    day first finish tasks

--- 2024/first/first_challenge.py
+++ 2024/first/first_challenge.py
@@ -6,6 +6,8 @@ second_list = []
 
 sum_distances = 0
 
+dict_right_distances = {}
+
 with open('input1.txt') as file:
     for line in file:
         first_number, second_number = line.split()
@@ -15,6 +17,12 @@ with open('input1.txt') as file:
         second_distance += second_number
         first_list.append(first_number)
         second_list.append(second_number)
+        
+        if second_number in dict_right_distances:
+            dict_right_distances[second_number] = dict_right_distances[second_number]+1
+        else:
+            dict_right_distances[second_number] = 1
+
     first_list.sort()
     second_list.sort()
 
@@ -24,4 +32,7 @@ with open('input1.txt') as file:
     print(sum_distances)
 print(second_distance-first_distance)
 # dlaczego suma nie działa? tyle wyszło: 1689836 - jest różnica, nie można tak zrobić jeżeli lewa wyprzedza prawą.
-
+similarity_sum = 0
+for i in first_list:
+    similarity_sum += i*dict_right_distances.get(i,0)
+print(similarity_sum)
