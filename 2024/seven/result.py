79c35ff23ac30e696d80137fe4fdd97925076c95
Magdalena Kuna <madziakunia@gmail.com>
2024-12-07 23:03:11 +0100

    day 7, task 1, done

---
+++ 2024/seven/result.py
@@ -0,0 +1,34 @@
+
+total_result = 0
+
+def operations(first, second, op):
+    #print(first, second,op)
+    if op == '+':
+        return first + second
+    return first*second
+
+with open('input.txt') as file:
+    for line in file:
+        part = line.find(':')
+        result = line[:part]
+        result = int(result)
+        rest = line[part+1:]
+        equation = []
+        equation_final= []
+        for number in rest.split():
+            number = int(number)
+            if not equation:
+                equation.append(number)
+                continue
+            for part in equation:
+                r1 = operations(part, number, '+')
+                r2 = operations(part, number,'*')
+                #print(r1,r2)
+                equation_final.append(r1)
+                equation_final.append(r2)
+            equation = equation_final[::]
+        #print(result, equation_final)
+        if result in equation_final:
+            total_result += result
+    print(total_result)
+
