b0a438fa26b5dcf9d8cdd7ebbacb814f8bd1944e
Magdalena Kuna <madziakunia@gmail.com>
2024-12-08 00:31:31 +0100

    day 7, task 2, undone

--- 2024/seven/result.py
+++ 2024/seven/result.py
@@ -5,6 +5,8 @@ def operations(first, second, op):
     #print(first, second,op)
     if op == '+':
         return first + second
+    if op == '||':
+        return int(str(first) + str(second))
     return first*second
 
 with open('input.txt') as file:
@@ -23,12 +25,15 @@ with open('input.txt') as file:
             for part in equation:
                 r1 = operations(part, number, '+')
                 r2 = operations(part, number,'*')
+                r3 = operations(part, number,'||')
                 #print(r1,r2)
                 equation_final.append(r1)
                 equation_final.append(r2)
+                equation_final.append(r3)
             equation = equation_final[::]
         #print(result, equation_final)
         if result in equation_final:
+            print(result)
             total_result += result
     print(total_result)
 
